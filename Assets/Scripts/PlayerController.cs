1a685c0cea52760fb4aa429f16b6b459d13a812c
judymute <julie.lam1181@gmail.com>
2024-02-19 21:07:08 -0500

    further commits

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     public float speed = 5.0f;
-    public float gravity = -9.81f;
+    public float gravity = 9.81f;
     public float jumpHeight = 2.0f;
     private Vector3 velocity;
     public Transform groundCheck;
@@ -25,24 +25,31 @@ public class PlayerController : MonoBehaviour
         // If on the ground and falling, reset fall velocity
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f; // Small downward force to ensure the character stays grounded
+            velocity.y = 0.0f; // Small downward force to ensure the character stays grounded
         }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        if (isGrounded) 
+        {
+            if (Input.GetButton("Jump"))
+            {
+                Debug.Log("Jump button pressed");
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+        }
+
+
+        Vector3 move = (transform.right * x + transform.forward * z).normalized;
 
         controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime); // Apply gravity
     }
+
 
 }
