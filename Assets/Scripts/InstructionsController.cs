9b61daab4fe2fdd2ffb9783f27cd9269542c66ed
judymute <julie.lam1181@gmail.com>
2024-04-16 17:08:34 -0400

    Added Start Button
    swapped the last next button with the start button in the last instruction video

--- Assets/Scripts/InstructionsController.cs
+++ Assets/Scripts/InstructionsController.cs
@@ -5,44 +5,51 @@ using UnityEngine.Video;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
-
 public class InstructionsController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public VideoClip[] videoClips;
     public Button nextVideoButton;
+    public Button startButton;  // This is the Start button which replaces the Next button on the last video
 
     private int currentVideoIndex = 0;
 
-
     void Start()
     {
-
         // Initially play the first video if not already playing
-        if (!videoPlayer.isPlaying)
-        {
-            videoPlayer.clip = videoClips[currentVideoIndex];
-            videoPlayer.Play();
-        }
+        videoPlayer.clip = videoClips[currentVideoIndex];
+        videoPlayer.Play();
 
-        // Setting up the button event listener
+        // Setting up the button event listeners
         nextVideoButton.onClick.AddListener(PlayNextVideo);
+        startButton.onClick.AddListener(LoadNextScene);
+
+        // Initially hide the start button
+        startButton.gameObject.SetActive(false);
     }
 
     public void PlayNextVideo()
     {
-        if (currentVideoIndex < videoClips.Length - 1)
+        // Increment the video index
+        currentVideoIndex++;
+
+        if (currentVideoIndex < videoClips.Length)
         {
-            currentVideoIndex++;
             videoPlayer.clip = videoClips[currentVideoIndex];
             videoPlayer.Play();
         }
-        else
+
+        // Check if the current video is the last one
+        if (currentVideoIndex == videoClips.Length - 1)
         {
-             SceneManager.LoadScene("Level 1");
-            
+            // This is the last video, so switch buttons
+            nextVideoButton.gameObject.SetActive(false);
+            startButton.gameObject.SetActive(true);
         }
     }
 
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
 }
